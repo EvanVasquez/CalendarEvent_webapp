ee23529a3033e5dd65eade12e18ef9bf6d155272
Evan Vasquez <32246279+EvanVasquez@users.noreply.github.com>
2018-06-11 22:17:33 -0400

    All questions in their own file.

---
+++ Q1.java
@@ -0,0 +1,27 @@
+//Evan Vasquez
+
+
+
+public class Q1{
+    public static void main(String[] args) {
+        String s ="good";
+        String t ="odg";
+
+        sortByStrings(s,t);
+    }
+
+    public static void sortByStrings(String s,String t){
+        String out = "";
+        for (int i = 0; i < t.length(); i++){
+            for (int p = 0; p < s.length(); p++){
+                if(t.charAt(i) == s.charAt(p)){
+                    out += s.charAt(p);
+                }
+            }
+        }
+        System.out.println(out);
+    }
+
+
+
+}
